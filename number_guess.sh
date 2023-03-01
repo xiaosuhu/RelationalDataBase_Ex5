c82f3ba422b493caf8c7722302b1ccfee48090db
xiaosuhu <xiaosuhu@outlook.com>
2023-03-01 17:13:11 +0000

    test: final stage test

--- number_guess.sh
+++ number_guess.sh
@@ -15,14 +15,13 @@ else
   echo $USER_QUERY | while read USERNAME BAR GAME_PLAYED BAR BEST_GAME
   do
     echo "Welcome back, $USERNAME! You have played $GAME_PLAYED games, and your best game took $BEST_GAME guesses."
+    let GAME_PLAYED=$GAME_PLAYED+1
+    ADD_GAME_NUM=$($PSQL "update usert set game_played=$GAME_PLAYED where username='$USERNAME'")
   done
 fi
 
 NUMBER=$(( ( RANDOM % 1000 )  + 1 ))
 echo $NUMBER
-
-let GAME_PLAYED=$GAME_PLAYED+1
-ADD_GAME_NUM=$($PSQL "update usert set game_played=$GAME_PLAYED where username='$USERNAME'")
 
 GUESS_TIMES=0
 echo "Guess the secret number between 1 and 1000:"
@@ -43,13 +42,18 @@ do
       let GUESS_TIMES=$GUESS_TIMES+1
     else
       let GUESS_TIMES=$GUESS_TIMES+1
-      echo "You guessed it in $GUESS_TIMES tries. The secret number was $NUMBER. Nice job!"
+ 
       # Compare with the previous best_game record and write in database
-     
-      if [[ $GUESS_TIMES -le $BEST_GAME ]]
-      then
-        NEW_BEST_GAME=$($PSQL "update usert set best_game=$GUESS_TIMES where username='$USERNAME'")
-      fi
+      echo $USER_QUERY | while read USERNAME BAR GAME_PLAYED BAR BEST_GAME
+      do
+        if [[ $GUESS_TIMES < $BEST_GAME ]]
+        then
+          NEW_BEST_GAME=$($PSQL "update usert set best_game=$GUESS_TIMES where username='$USERNAME'")
+        fi
+      done
+        
+      echo "You guessed it in $GUESS_TIMES tries. The secret number was $NUMBER. Nice job!"
+
       break
     fi
   fi
