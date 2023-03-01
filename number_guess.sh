d572cd7788acd105845189d29a594a4bddbeffa1
xiaosuhu <xiaosuhu@outlook.com>
2023-03-01 01:34:22 +0000

    feat:Database added and some minor progress to the sh

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+NUMBER=$(( ( RANDOM % 1000 )  + 1 ))
+echo $NUMBER
+
+echo "Enter your username:"
+read USERNAME
+
+USER_QUERY=$($PSQL "")
